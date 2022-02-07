5143031369c9bfed9a006a01e8e9e67750973cbb
benabdellah omar elfarouk <benabdellah995@gmail.com>
2022-02-07 11:48:06 +0100

    static file and PK Whitenoise

--- DjangoQuizBrenco/settings.py
+++ DjangoQuizBrenco/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,12 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 ##Added Manually
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
 # Managing Media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 MEDIA_URL = '/media/'
